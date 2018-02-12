df6e52975c16804a226957271c7d58c46443bcef
Wilk Maia <wilkmaia@gmail.com>
2018-02-12 16:57:48 -0300

    core: print errors to stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Error on initialization\n{}", err);
+        eprintln!("Error on initialization\n{}", err);
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        println!("Error running application\n{}", err);
+        eprintln!("Error running application\n{}", err);
         process::exit(1);
     }
 }
